bcd789eaa83bcf0321c882d2a3a8665e7a204b72
SEONGJUSEO <canwefly89@gmail.com>
2019-10-29 02:44:21 +0900

    Fill event js

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const canvas = document.getElementById("jsCanvas"); //canvas는 html 기능 pixe
 const ctx = canvas.getContext("2d"); // 2d로 작업
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 600; //html canvas에 사이즈를 알려줌
-canvas.height = 600;
+const INITIAL_COLOR = "#2c2c2c"; //반복하게 된다면, 이런식으로 변수 생성
+const CANVAS_SIZE = 600;
 
-ctx.strokeStyle = "#2c2c2c"; //기본 선색깔
+canvas.width = CANVAS_SIZE; //html canvas에 사이즈를 알려줌
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; //기본 선색깔
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; //기본 선굵기
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
@@ -34,6 +40,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor; //evnt.target : 해당하는 event DOM 객체 불러오기
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -42,11 +49,29 @@ function handleRangeChange(event) {
   ctx.lineWidth = size;
 }
 
+function handleModeClick() {
+  if (filling === true) {
+    //처음에 false 값이기 때문에
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(
@@ -56,4 +81,8 @@ Array.from(colors).forEach(
 if (range) {
   //물론 여기서 range는 늘 true지만, safe/check를 위해서 if로 한 번 감싸주기 (element가 없을 경우의 전체 에러 방지)
   range.addEventListener("input", handleRangeChange); //html의 input 요소 (bar)
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
